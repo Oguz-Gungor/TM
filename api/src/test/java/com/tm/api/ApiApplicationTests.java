1f4df58106d5d2bc85d04c074ce5c1783255643b
Oğuz Güngör <m.oguzgungor60.57@gmail.com>
2024-02-10 21:42:32 +0300

    initial

---
+++ api/src/test/java/com/tm/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tm.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
