41d873bc1b098d542397efffcc9232247beb33f2
Oğuz Güngör <m.oguzgungor60.57@gmail.com>
2024-02-12 03:56:39 +0300

    Add swagger config to test authorized APIs

---
+++ api/src/main/java/com/tm/api/configuration/SwaggerConfiguration.java
@@ -0,0 +1,36 @@
+package com.tm.api.configuration;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SwaggerConfiguration {
+
+    /**
+     * Configuration of swagger via OpenAPI bean
+     *
+     * @return OpenAPI model with configured swagger options
+     */
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI().addSecurityItem(new SecurityRequirement().
+                        addList("Bearer Authentication"))
+                .components(new Components().addSecuritySchemes
+                        ("Bearer Authentication", createAPIKeyScheme()));
+    }
+
+    /**
+     * To create bearer security scheme for swagger config
+     *
+     * @return security scheme with bearer JWT
+     */
+    private SecurityScheme createAPIKeyScheme() {
+        return new SecurityScheme().type(SecurityScheme.Type.HTTP)
+                .bearerFormat("JWT")
+                .scheme("bearer");
+    }
+}
