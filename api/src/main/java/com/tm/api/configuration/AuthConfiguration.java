8c15371390b6f7dfc7c92616ed2102471b93de7d
Oğuz Güngör <m.oguzgungor60.57@gmail.com>
2024-02-12 03:52:44 +0300

    Authorized paths have been added to Authconfiguration

--- api/src/main/java/com/tm/api/configuration/AuthConfiguration.java
+++ api/src/main/java/com/tm/api/configuration/AuthConfiguration.java
@@ -1,5 +1,7 @@
 package com.tm.api.configuration;
 
+import com.tm.api.constants.ApiPaths;
+import com.tm.api.constants.UserRoleKeys;
 import com.tm.api.filter.SecurityFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -18,20 +20,46 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @EnableWebSecurity
 public class AuthConfiguration {
 
+    /**
+     * Security filter Bean
+     */
     private final SecurityFilter securityFilter;
 
     public AuthConfiguration(SecurityFilter securityFilter) {
         this.securityFilter = securityFilter;
     }
 
-    public static final String ADMIN_ROLE = "ROLE_ADMIN";
-    public static final String USER_ROLE = "ROLE_USER";
+    /**
+     * APIs that does not need any authority to be used
+     */
     private static final String[] AUTH_WHITELIST = {
             "/v3/api-docs/**",
             "/swagger-ui/**",
-            "/auth/**"
+            ApiPaths.AUTH + "/**",
+            ApiPaths.HEALTH_CHECK
     };
 
+    /**
+     * APIs only to be used by users with Admin Authority
+     */
+    private static final String[] ADMIN_API = {
+            ApiPaths.ADMIN + "/**"
+    };
+
+    /**
+     * APIs can be used by signedIn users
+     */
+    private static final String[] USER_API = {
+            ApiPaths.RESOURCES + "/**"
+    };
+
+    /**
+     * Bean to configure http security filter operations
+     *
+     * @param httpSecurity builder to build security filter chain
+     * @return Security filter chain to be used in spring security flow
+     * @throws Exception from csrf
+     */
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity
@@ -39,17 +67,30 @@ public class AuthConfiguration {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers(AUTH_WHITELIST).permitAll()
+                        .requestMatchers(ADMIN_API).hasAuthority(UserRoleKeys.ADMIN_ROLE)
+                        .requestMatchers(USER_API).hasAnyAuthority(UserRoleKeys.ADMIN_ROLE, UserRoleKeys.USER_ROLE)
                         .anyRequest().authenticated())
                 .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 
+    /**
+     * Authentication Manager bean to be used spring security flow
+     *
+     * @param authenticationConfiguration existing cınfiguration
+     * @return configured authenticationManager
+     * @throws Exception from getAuthenticationManager
+     */
     @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
-            throws Exception {
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
         return authenticationConfiguration.getAuthenticationManager();
     }
 
+    /**
+     * Password Encoder Bean to be used for spring security flow
+     *
+     * @return BCryptPasswordEncoder
+     */
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
